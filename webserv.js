44dc0ff2ae861c51d7f9aaa817b090f642ea4142
DESKTOP-1L38NVA\Nilesh Devdas <nilesh.devdas@gmail.com>
2018-06-26 13:00:12 -0400

    New Changes

--- webserv.js
+++ webserv.js
@@ -1,4 +1,7 @@
 var http = require("http");
+var events = require("events");
+
+var EventEmmiter = events();
 var Server = http.createServer(function(request,response){
   switch(request.url){
       case "/login":
@@ -13,7 +16,13 @@ var Server = http.createServer(function(request,response){
 });
 Server.listen(5000);
 function handleLogin(request, response){
-   response.end("You Finished Login");
+  // json objects
+   var user = {};
+   user.username = "nilesh";
+   user.password = "password"
+   response.setHeader("content-type", "application/json");
+   response.end(JSON.stringify(user));
+
 }
 function handleLogout(request, response){
   response.end("You Finished Logout");
